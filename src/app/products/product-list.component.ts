0575cf5a3bf7519208cde083c2920b8b0ae99c01
felipebelarmino <epilefgomes1@gmail.com>
2021-04-30 20:30:16 -0300

    Add api/products mock, add directive *ngIf products.lengh into component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,4 +4,31 @@ import { Component } from '@angular/core';
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+  pageTitle: string = 'Product List';
+  products: any[] = [
+    {
+      productId: 2,
+      productName: 'Galaxy A31',
+      productCode: 'SM-A315GZBKZTO',
+      releaseDate: 'March 18, 2021',
+      description: 'Quádrupla 48MP e Bateria de 5000mAh.',
+      price: 1699.99,
+      starRating: 4.5,
+      imageUrl:
+        'https://samsungbr.vtexassets.com/arquivos/ids/313828-1200-auto?width=1200&height=auto&aspect=true',
+    },
+    {
+      productId: 5,
+      productName: 'Galaxy S21',
+      productCode: 'SM-G996BZKJZTO',
+      releaseDate: 'March 18, 2021',
+      description:
+        'O display com cores mais vivas e brilhantes em um smartphone.',
+      price: 5699.99,
+      starRating: 4.6,
+      imageUrl:
+        'https://samsungbr.vtexassets.com/arquivos/ids/315731-1200-auto?width=1200&height=auto&aspect=true',
+    },
+  ];
+}

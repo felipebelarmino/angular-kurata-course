0f4c231b4e11ca3e725ffe9afd570dad3aafd46a
felipebelarmino <epilefgomes1@gmail.com>
2021-05-01 18:40:49 -0300

    Add routes home and product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-    <div>
-      <h1>{{ title }}</h1>
-      <pm-products></pm-products>
-    </div>
+    <nav class="column">
+      <a class="navbar-brand">{{ title }}</a>
+      <ul class="nav nav-pills">
+        <li><a class="nav-link" [routerLink]="['/welcome']">Home</a></li>
+        <li>
+          <a class="nav-link" [routerLink]="['/products']">Product List</a>
+        </li>
+      </ul>
+      <div class="container">
+        <router-outlet></router-outlet>
+      </div>
+    </nav>
   `,
 })
 export class AppComponent {
   title: string = 'Infoway - Admin';
 }
+
+//navbar navbar-expand navbar-light bg-light

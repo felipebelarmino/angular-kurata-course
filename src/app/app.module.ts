51a1513b4fc85b609d0b37d37d926b1689caeb96
felipebelarmino <epilefgomes1@gmail.com>
2021-05-03 19:03:38 -0300

    web.config deploy test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,28 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { CovertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import localePt from '@angular/common/locales/pt';
-import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    ProductListComponent,
-    CovertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      {
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent,
-      },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
     ]),
+    ProductModule,
   ],
   providers: [
     {
--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -15,7 +15,20 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `:${id}`;
+
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2021',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/leaf_rake.png'
+    };
   }
+
+
 
   onBack(): void {
     this.router.navigate(['/products']);
